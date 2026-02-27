3e1bb13ff78512975f81681f75219f2aacaa14f8
Youssef elshaer <youssefrabih49@gmail.com>
2026-02-27 16:08:42 +0200

    Add files via upload

---
+++ mysql-projects/project1/problem1.session.sql
@@ -0,0 +1,9 @@
+ SELECT * FROM stu;
+ SELECT name, grade FROM stu;
+ SELECT * FROM stu WHERE age > 20;
+ SELECT * FROM stu WHERE class = '1A';
+ SELECT * FROM stu WHERE grade > 85;
+UPDATE stu
+SET grade = 85
+WHERE name = 'Omar Samir';
+ SELECT * FROM stu WHERE city = 'Cairo';
---
+++ mysql-projects/project3/book.session.sql
@@ -0,0 +1,6 @@
+SELECT * FROM books;
+SELECT book_name, author FROM books;
+SELECT * FROM books WHERE price < 400;
+SELECT * FROM books WHERE category = 'Programming';
+SELECT * FROM books 
+WHERE pages BETWEEN 300 AND 500;
